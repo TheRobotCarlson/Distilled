4c3c0b85619aaa134f97b6fa01579946c8e253d6
Brian Carlson <briancarlson6174@gmail.com>
2019-04-20 14:59:21 -0400

    Initial application generated by JHipster-5.8.2

--- src/main/java/com/therobotcarlson/distilled/config/CacheConfiguration.java
+++ src/main/java/com/therobotcarlson/distilled/config/CacheConfiguration.java
@@ -38,6 +38,32 @@ public class CacheConfiguration {
             cm.createCache(com.therobotcarlson.distilled.domain.User.class.getName(), jcacheConfiguration);
             cm.createCache(com.therobotcarlson.distilled.domain.Authority.class.getName(), jcacheConfiguration);
             cm.createCache(com.therobotcarlson.distilled.domain.User.class.getName() + ".authorities", jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.Grain.class.getName(), jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.Yeast.class.getName(), jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.MashbillGrain.class.getName(), jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.MashbillGrain.class.getName() + ".mashbills", jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.Customer.class.getName(), jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.Customer.class.getName() + ".mashbills", jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.Customer.class.getName() + ".barrels", jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.Customer.class.getName() + ".schedules", jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.Spirit.class.getName(), jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.Mashbill.class.getName(), jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.Mashbill.class.getName() + ".barrels", jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.Mashbill.class.getName() + ".schedules", jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.Mashbill.class.getName() + ".grainCounts", jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.Warehouse.class.getName(), jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.Warehouse.class.getName() + ".barrels", jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.Warehouse.class.getName() + ".schedules", jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.Barrel.class.getName(), jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.Schedule.class.getName(), jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.Schedule.class.getName() + ".batches", jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.Batch.class.getName(), jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.Batch.class.getName() + ".barrels", jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.ProductionSchedule.class.getName(), jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.GrainForecast.class.getName(), jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.ProductionSummary.class.getName(), jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.YeastSummary.class.getName(), jcacheConfiguration);
+            cm.createCache(com.therobotcarlson.distilled.domain.MasterSummary.class.getName(), jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,66 @@ import { RouterModule } from '@angular/router';
 @NgModule({
     imports: [
         RouterModule.forChild([
+            {
+                path: 'grain',
+                loadChildren: './grain/grain.module#DistilledGrainModule'
+            },
+            {
+                path: 'yeast',
+                loadChildren: './yeast/yeast.module#DistilledYeastModule'
+            },
+            {
+                path: 'mashbill-grain',
+                loadChildren: './mashbill-grain/mashbill-grain.module#DistilledMashbillGrainModule'
+            },
+            {
+                path: 'customer',
+                loadChildren: './customer/customer.module#DistilledCustomerModule'
+            },
+            {
+                path: 'spirit',
+                loadChildren: './spirit/spirit.module#DistilledSpiritModule'
+            },
+            {
+                path: 'mashbill',
+                loadChildren: './mashbill/mashbill.module#DistilledMashbillModule'
+            },
+            {
+                path: 'warehouse',
+                loadChildren: './warehouse/warehouse.module#DistilledWarehouseModule'
+            },
+            {
+                path: 'barrel',
+                loadChildren: './barrel/barrel.module#DistilledBarrelModule'
+            },
+            {
+                path: 'schedule',
+                loadChildren: './schedule/schedule.module#DistilledScheduleModule'
+            },
+            {
+                path: 'batch',
+                loadChildren: './batch/batch.module#DistilledBatchModule'
+            },
+            {
+                path: 'production-schedule',
+                loadChildren: './production-schedule/production-schedule.module#DistilledProductionScheduleModule'
+            },
+            {
+                path: 'grain-forecast',
+                loadChildren: './grain-forecast/grain-forecast.module#DistilledGrainForecastModule'
+            },
+            {
+                path: 'production-summary',
+                loadChildren: './production-summary/production-summary.module#DistilledProductionSummaryModule'
+            },
+            {
+                path: 'yeast-summary',
+                loadChildren: './yeast-summary/yeast-summary.module#DistilledYeastSummaryModule'
+            },
+            {
+                path: 'master-summary',
+                loadChildren: './master-summary/master-summary.module#DistilledMasterSummaryModule'
+            }
             /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
         ])
     ],
